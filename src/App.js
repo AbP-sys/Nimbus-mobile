cdd7c31894baf13e3a5776ff62d4b9843be30921
AbP-sys <abhishekpagadala@gmail.com>
2024-06-13 15:47:12 +0530

    added status bar configuration for mobile devices

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Login from './components/Login';
 import {ChakraProvider} from '@chakra-ui/react'
+import { StatusBar, Style } from '@capacitor/status-bar';
 
 function App() {
+  useEffect(() => {
+    const setStatusBar = async () => {
+      await StatusBar.setStyle({ style: Style.Light });
+      await StatusBar.setBackgroundColor({ color: '#ffffff' }); // Set this to match your app's background color
+    };
+    setStatusBar();
+  }, []);
+
   return (
     <ChakraProvider>
         <div className="App">
